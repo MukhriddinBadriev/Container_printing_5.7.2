08e3222cfaf5422a11bd03c5b5740e0167f1a690
Mukhriddin Badriev <muhriddinbadriyev7@gmail.com>
2022-11-12 00:07:03 +0300

    Container printing

---
+++ 5.7.2/5.7.2.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+
+#include <string>
+#include <set>
+#include <list>
+#include <vector>
+
+template<typename T>
+void print_container(T t) {
+
+    for (const auto& elem : t) {
+        std::cout << elem << ' ';
+    } std::cout << "\n\n";
+}
+
+int main()
+{
+    std::set<std::string> test_set = { "one", "two", "three", "four" };
+    print_container(test_set);
+
+    std::list<std::string> test_list = { "one", "two", "three", "four" };
+    print_container(test_list);
+
+    std::vector<std::string> test_vector = { "one", "two", "three", "four" };
+    print_container(test_vector);
+
+    return 0;
+};
